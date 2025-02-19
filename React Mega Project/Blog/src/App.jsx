2c6c85fde2a41d72e416363803c37349b474de37
Hetvi-KE <hetvi.chudasama@kernelequity.com>
2025-02-19 17:57:01 +0530

    Update App.jsx

--- React Mega Project/Blog/src/App.jsx
+++ React Mega Project/Blog/src/App.jsx
@@ -1,11 +1,23 @@
+import { useState,useDispatch, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import AuthService from "./appwrite/auth";
+// import { login, logout } from './features/auth/authSlice';
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    AuthServicegetCurrentUser().then((userData) =>{}).finally()
+  },[])
 
   return (
     <>
      <h1>hello react i am learning react </h1>
+    
     </>
   )
 }
